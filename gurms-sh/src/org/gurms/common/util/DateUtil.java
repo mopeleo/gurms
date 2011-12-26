17e036b497832088740df8a16cbce82cd21adafb
leo <mopleo@gmail.com>
2011-12-26 10:41:31 +0800

    Signed-off-by: leo <mopleo@gmail.com>

--- gurms-sh/src/org/gurms/common/util/DateUtil.java
+++ gurms-sh/src/org/gurms/common/util/DateUtil.java
@@ -1,7 +1,5 @@
 package org.gurms.common.util;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -9,34 +7,13 @@ import java.util.Date;
 
 import org.apache.commons.lang.StringUtils;
 
-public class FormatUtil {
+public class DateUtil {
 
-	private static final String symbol = "\r\n\t";
 	public static final String pattern_time = "HHmmss";
 	public static final String pattern_date = "yyyyMMdd";
 	public static final String pattern_fulltime = "yyyyMMddHHmmss";
 
-	private FormatUtil(){}
-	
-	public static String bean2string(Object obj){
-		StringBuffer sb = new StringBuffer();
-		Class c = obj.getClass();
-		sb.append(c).append(" : ").append(symbol);
-		Field[] fields = c.getDeclaredFields();
-		try{
-			for(Field f : fields){
-				String field = f.getName();
-				String methodName = "get" + field.substring(0,1).toUpperCase()
-						+ field.substring(1);
-				Method method = c.getMethod(methodName);
-				sb.append(field).append(" = ");
-				sb.append(method.invoke(obj)).append(symbol);
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		return sb.toString();
-	}
+	private DateUtil(){}
 	
 	public static String getCurrentTime(){
 		return getCurrentTime(pattern_time);
