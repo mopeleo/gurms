4a6c2f282537869984f1f61ac9242500d9254f85
Leo <Leo@Blue>
2012-01-21 21:17:14 +0800

    Signed-off-by: Leo <Leo@Blue>

--- gurms-sh/src/org/gurms/common/util/IDCardUtil.java
+++ gurms-sh/src/org/gurms/common/util/IDCardUtil.java
@@ -139,7 +139,7 @@ public class IDCardUtil {
 	}
 	
 	public static void main(String[] args) {
-		String idcard = "421003197906020535";
+		String idcard = "111111111111111111";
 		System.out.println(validateIDCard(idcard));
 	}
 }
