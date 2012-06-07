4a977e78ffb2626792f5ce9f62cf3253fa9d1a1f
Leo <Leo@Blue>
2012-06-07 17:57:11 +0800

    Signed-off-by: Leo <Leo@Blue>

---
+++ gurms-sh/src/org/gurms/dao/hibernate/system/SysAccessoryDao.java
@@ -0,0 +1,9 @@
+package org.gurms.dao.hibernate.system;
+
+import org.gurms.dao.hibernate.HibernateDao;
+import org.gurms.entity.system.SysAccessory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SysAccessoryDao extends HibernateDao<SysAccessory> {
+}
