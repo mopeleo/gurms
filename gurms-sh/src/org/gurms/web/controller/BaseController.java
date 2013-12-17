05dce1f3fdfa29fed1131307fd1db63acbd31457
Leo <Leo@Blue>
2013-12-17 08:24:04 +0800

    Signed-off-by: Leo <Leo@Blue>

--- gurms-sh/src/org/gurms/web/controller/BaseController.java
+++ gurms-sh/src/org/gurms/web/controller/BaseController.java
@@ -1,9 +1,13 @@
 package org.gurms.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.gurms.common.config.GlobalConfig;
+import org.gurms.entity.PageRequest;
 import org.gurms.entity.PageResult;
+import org.gurms.entity.system.SysUserConfig;
+import org.gurms.web.WebConstants;
 import org.gurms.web.spring.IntPropertyBinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,5 +47,18 @@ public abstract class BaseController {
 			path = ctxPath + path;
 		}
 		return path;
+	}
+	
+	//设置个性化服务
+	protected void setIndividuation(HttpServletRequest request,PageRequest page){
+		HttpSession session = request.getSession();
+		SysUserConfig config = (SysUserConfig)session.getAttribute(WebConstants.S_KEY_USERCONFIG);
+		if(config != null){
+			//若列表的初始条数没有改动过，则替换为个性化列表条数
+			int pageSize = config.getPagesize();
+			if(page.getPageSize() == page.MIN_PAGESIZE){
+				page.setPageSize(pageSize);
+			}			
+		}
 	}
 }
